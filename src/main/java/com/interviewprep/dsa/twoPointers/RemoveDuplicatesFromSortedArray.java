fecb203df4a66048abbd490956910cb793da363e
Rajesh <beherarajesh90@gmail.com>
2026-03-18 21:22:56 +0530

    pattern: two pointers -> Remove DuplicatesFrom Sorted Array

---
+++ src/main/java/com/interviewprep/dsa/twoPointers/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,21 @@
+package com.interviewprep.dsa.twoPointers;
+
+public class RemoveDuplicatesFromSortedArray {
+    public int removeDuplicates(int[] nums) {
+        int nextEleInd = 1;
+        for(int i=1;i<nums.length;i++){
+            if(nums[nextEleInd-1]!=nums[i]){
+                nums[nextEleInd]=nums[i];
+                nextEleInd++;
+            }
+        }
+        return nextEleInd;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,1,1,2,2,3};
+        RemoveDuplicatesFromSortedArray removeDuplicatesFromSortedArray = new RemoveDuplicatesFromSortedArray();
+        int result = removeDuplicatesFromSortedArray.removeDuplicates(nums);
+        System.out.println(result);
+    }
+}
