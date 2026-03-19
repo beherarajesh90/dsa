67dede7bbfa19ed9ea4538bb4f7bc659c0bf6bf2
Rajesh <beherarajesh90@gmail.com>
2026-03-19 16:32:42 +0530

    pattern: two pointers -> SortColors Or DutchNationalFlagProblem

---
+++ src/main/java/com/interviewprep/dsa/twoPointers/SortColorsOrDutchNationalFlagProblem.java
@@ -0,0 +1,34 @@
+package com.interviewprep.dsa.twoPointers;
+
+import java.util.Arrays;
+
+public class SortColorsOrDutchNationalFlagProblem {
+    public int[] sortColors(int[] arr){
+        int low=0, high=arr.length-1;
+        for(int i=0;i<=high;){
+            if(arr[i]==0){
+                swap(arr,i,low);
+                i++;
+                low++;
+            } else if(arr[i]==1){
+                i++;
+            } else{
+                swap(arr,i,high);
+                high--;
+            }
+        }
+        return arr;
+    }
+    private void swap(int[] arr, int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{2,0,1};
+        SortColorsOrDutchNationalFlagProblem sortColorsOrDutch_National_Flag_Problem = new SortColorsOrDutchNationalFlagProblem();
+        int[] result = sortColorsOrDutch_National_Flag_Problem.sortColors(arr);
+        System.out.println(Arrays.toString(result));
+    }
+}
