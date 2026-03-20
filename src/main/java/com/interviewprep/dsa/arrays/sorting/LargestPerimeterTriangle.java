9559689bef4631c3f12a08ae284c701c2fd4d1ed
Rajesh <beherarajesh90@gmail.com>
2026-03-20 16:34:06 +0530

    pattern: sorting -> Largest Perimeter Triangle

---
+++ src/main/java/com/interviewprep/dsa/arrays/sorting/LargestPerimeterTriangle.java
@@ -0,0 +1,17 @@
+package com.interviewprep.dsa.arrays.sorting;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+public class LargestPerimeterTriangle {
+    public int largestPerimeter(int[] nums) {
+        Arrays.sort(nums);
+        for (int i=nums.length-1; i>=2; i--){
+            if(nums[i-1]+nums[i-2] > nums[i]){
+                return nums[i] + nums[i-1] + nums[i-2];
+            }
+        }
+        return 0;
+    }
+}
