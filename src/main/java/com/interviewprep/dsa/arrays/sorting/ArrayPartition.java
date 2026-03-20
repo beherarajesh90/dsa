6e9f54ee04d90677e557fa6a6c089bde8c2e7d89
Rajesh <beherarajesh90@gmail.com>
2026-03-20 16:11:28 +0530

    pattern: sorting -> Array Partition

---
+++ src/main/java/com/interviewprep/dsa/arrays/sorting/ArrayPartition.java
@@ -0,0 +1,14 @@
+package com.interviewprep.dsa.arrays.sorting;
+
+import java.util.Arrays;
+
+public class ArrayPartition {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int maxSum = 0;
+        for(int i=0; i<nums.length; i+=2){
+            maxSum+=nums[i];
+        }
+        return maxSum;
+    }
+}
