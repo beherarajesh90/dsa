2be42250561724c37b04a15a54745beb3920f722
Rajesh <beherarajesh90@gmail.com>
2026-04-01 22:52:22 +0530

    binary search: basic binary search -> Find Smallest Letter Greater Than Target

---
+++ src/main/java/com/interviewprep/dsa/binarySearch/FindSmallestLetterGreaterThanTarget.java
@@ -0,0 +1,23 @@
+package com.interviewprep.dsa.binarySearch;
+
+public class FindSmallestLetterGreaterThanTarget {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int left=0, right=letters.length-1;
+        char result=letters[0];
+        while (left<=right){
+            int mid = (left+right)/2;
+            if(letters[mid]>target){
+                right = mid-1;
+                result=letters[mid];
+            } else{
+                left = mid+1;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        char[] chars = {'c','f','j'};
+        System.out.println(new FindSmallestLetterGreaterThanTarget().nextGreatestLetter(chars, 'c'));
+    }
+}
