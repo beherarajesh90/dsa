a17e5e1fed996b09cbda89ef573e1f940d87e54b
Rajesh <beherarajesh90@gmail.com>
2026-04-01 23:13:10 +0530

    binary search: basic binary search -> Valid Perfect Square

---
+++ src/main/java/com/interviewprep/dsa/binarySearch/ValidPerfectSquare.java
@@ -0,0 +1,20 @@
+package com.interviewprep.dsa.binarySearch;
+
+public class ValidPerfectSquare {
+    public boolean isPerfectSquare(int num) {
+        if (num<2) return true;
+        int left=1, right=num/2;
+        while (left<=right){
+            int mid = left+(right-left)/2;
+            long square = (long) mid*mid;
+            if(square==num){
+                return true;
+            } else if (square>num){
+                right=mid-1;
+            } else {
+                left = mid+1;
+            }
+        }
+        return false;
+    }
+}
