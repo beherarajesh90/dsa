393a3e8486f09cdf916f25354fc4ccdd453dc0ec
Rajesh <beherarajesh90@gmail.com>
2026-04-06 13:23:24 +0530

    binary search: counting occurences -> Find First and Last Position of Element in Sorted Array

---
+++ src/main/java/com/interviewprep/dsa/binarySearch/countingOccurences/FindFirstAndLastPositionOfElementInSortedArray.java
@@ -0,0 +1,38 @@
+package com.interviewprep.dsa.binarySearch.countingOccurences;
+
+public class FindFirstAndLastPositionOfElementInSortedArray {
+    public int[] searchRange(int[] nums, int target) {
+        int left = 0, right = nums.length-1;
+        int[] result = new int[]{-1,-1};
+
+        // find left index of the target
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            if(nums[mid] == target){
+                result[0] = mid;
+                right = mid - 1;
+            } else if(nums[mid] < target){
+                left = mid + 1;
+            } else{
+                right = mid - 1;
+            }
+        }
+
+        // find right index of the target
+        left = 0;
+        right = nums.length-1;
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            if(nums[mid] == target){
+                result[1] = mid;
+                left = mid + 1;
+            } else if(nums[mid] < target){
+                left = mid + 1;
+            } else{
+                right = mid - 1;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ src/main/java/com/interviewprep/dsa/binarySearch/countingOccurences/readme.md
@@ -0,0 +1,6 @@
+# Binary Search Pattern Identification
+
+- Do I need to find the **first** and **last** occurrences?
+- Is the data **sorted**?
+- Count some **frequency**.
+- Is there any place where the value changes from **true to false** or from **false to true**?
