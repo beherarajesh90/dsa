b8a79576a3f36939a6552d70469da751f6be2c32
Rajesh <beherarajesh90@gmail.com>
2026-04-07 22:37:17 +0530

    binary search: bitonic array search -> Find Peak Element

--- src/main/java/com/interviewprep/dsa/binarySearch/bitonicArraySearch/FindPeakElement.java
+++ src/main/java/com/interviewprep/dsa/binarySearch/bitonicArraySearch/FindPeakElement.java
@@ -1,5 +1,6 @@
 package com.interviewprep.dsa.binarySearch.bitonicArraySearch;
 
+//https://leetcode.com/problems/find-peak-element/description/
 public class FindPeakElement {
     public int findPeakElement(int[] nums) {
         int left = 0, right = nums.length - 1;
