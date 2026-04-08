1441894d94cae45b2d20391c6a22f1317cbe2ab6
Rajesh <beherarajesh90@gmail.com>
2026-04-08 21:47:01 +0530

    binary search: two pointers -> Is Subsequence

---
+++ src/main/java/com/interviewprep/dsa/strings/twoPointers/IsSubsequence.java
@@ -0,0 +1,11 @@
+package com.interviewprep.dsa.strings.twoPointers;
+
+public class IsSubsequence {
+    public boolean isSubsequence(String s, String t) {
+        int j=0;
+        for(int i=0; i<t.length(); i++){
+            if(j<s.length() && t.charAt(i) == s.charAt(j)) j++;
+        }
+        return s.length() == j;
+    }
+}
