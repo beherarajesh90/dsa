2cda0d66cf683ae99c42dd4d8539f98f3636a5a6
Rajesh <beherarajesh90@gmail.com>
2026-04-08 22:46:02 +0530

    binary search: two pointers -> Ransom Note

---
+++ src/main/java/com/interviewprep/dsa/strings/hashmaps/RansomNote.java
@@ -0,0 +1,17 @@
+package com.interviewprep.dsa.strings.hashmaps;
+
+public class RansomNote {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] freq = new int[26];
+        for(char ch: magazine.toCharArray()){
+            freq[ch - 'a']++;
+        }
+
+        for(char ch: ransomNote.toCharArray()){
+            if(freq[ch - 'a']>0) freq[ch-'a']--;
+            else return false;
+        }
+
+        return true;
+    }
+}
