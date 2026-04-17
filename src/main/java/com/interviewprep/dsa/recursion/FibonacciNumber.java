2c2d6f4c82c83017460803129b347d76d7d386c2
Rajesh <beherarajesh90@gmail.com>
2026-04-17 17:18:06 +0530

    recursion: regular expressions -> basic recursive functions

---
+++ src/main/java/com/interviewprep/dsa/recursion/FibonacciNumber.java
@@ -0,0 +1,21 @@
+package com.interviewprep.dsa.recursion;
+
+//https://leetcode.com/problems/fibonacci-number/description/
+public class FibonacciNumber {
+    public int fib(int n) {
+        if(n<=1) return n;
+        int a=0,b=1;
+        for(int i=2; i<=n; i++){
+            int curVal = a + b;
+            a = b;
+            b = curVal;
+        }
+        return b;
+    }
+
+    public int fibRec(int n) {
+        if(n == 0) return 0;
+        if(n == 1) return 1;
+        return fibRec(n-1) + fibRec(n-2);
+    }
+}
