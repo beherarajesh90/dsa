fd2c8e91db7312a658eb8004d4e1d1a1ae16ffb0
Rajesh <beherarajesh90@gmail.com>
2026-04-25 15:36:56 +0530

    linked list: dummy node technique -> MergeTwoSortedLists

---
+++ src/main/java/com/interviewprep/dsa/linkedList/dummyNodeTechnique/MergeTwoSortedLists.java
@@ -0,0 +1,23 @@
+package com.interviewprep.dsa.linkedList.dummyNodeTechnique;
+
+import com.interviewprep.dsa.linkedList.ListNode;
+
+//https://leetcode.com/problems/merge-two-sorted-lists/
+public class MergeTwoSortedLists {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(0);
+        ListNode tail = dummy;
+        while(list1!=null && list2!=null){
+            if(list1.val <= list2.val){
+                tail.next = new ListNode(list1.val);
+                list1 = list1.next;
+            } else{
+                tail.next = new ListNode(list2.val);
+                list2 = list2.next;
+            }
+            tail = tail.next;
+        }
+        tail.next = (list1!=null ? list1 : (list2!=null ? list2 : null));
+        return dummy.next;
+    }
+}
