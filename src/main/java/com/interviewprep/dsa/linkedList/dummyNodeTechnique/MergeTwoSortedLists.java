2a0db824a5fa76fdba02051ba1c071cb89e69373
Rajesh <beherarajesh90@gmail.com>
2026-04-26 12:19:44 +0530

    linked list: dummy node technique -> MergeTwoSortedLists

--- src/main/java/com/interviewprep/dsa/linkedList/dummyNodeTechnique/MergeTwoSortedLists.java
+++ src/main/java/com/interviewprep/dsa/linkedList/dummyNodeTechnique/MergeTwoSortedLists.java
@@ -20,4 +20,16 @@ public class MergeTwoSortedLists {
         tail.next = (list1!=null ? list1 : (list2));
         return dummy.next;
     }
+
+    public ListNode mergeTwoListsRecursive(ListNode list1, ListNode list2) {
+        if(list1 == null) return list2;
+        else if(list2 == null) return list1;
+        if(list1.val < list2.val){
+            list1.next = mergeTwoListsRecursive(list1.next, list2);
+            return list1;
+        } else{
+            list2.next = mergeTwoListsRecursive(list1, list2.next);
+            return list2;
+        }
+    }
 }
