611562342cb76bf54783bdf1f3ce6042c8d058eb
Rajesh <beherarajesh90@gmail.com>
2026-04-26 19:30:58 +0530

    linked list: recursion -> FlattenAMultilevelDoublyLinkedList

---
+++ src/main/java/com/interviewprep/dsa/linkedList/Node.java
@@ -0,0 +1,8 @@
+package com.interviewprep.dsa.linkedList;
+
+class Node {
+    public int val;
+    public Node prev;
+    public Node next;
+    public Node child;
+};
---
+++ src/main/java/com/interviewprep/dsa/linkedList/recursion/FlattenAMultilevelDoublyLinkedList.java
@@ -0,0 +1,62 @@
+package com.interviewprep.dsa.linkedList.recursion;
+
+public class FlattenAMultilevelDoublyLinkedList {
+    public Node flatten(Node head) {
+        if(head == null) return head;
+        Node cur = head;
+        while(cur!=null){
+
+            //if node has child, iterate throught the child
+            if(cur.child!=null){
+                //find child head
+                Node next = cur.next;
+                Node childHead = cur.child;
+                cur.next = childHead;
+                cur.child = null;
+                childHead.prev = cur;
+
+                //find child tail
+                Node childTail = cur;
+                while(childTail.next!=null){
+                    childTail = childTail.next;
+                }
+
+                if(next!=null){
+                    childTail.next = next;
+                    next.prev = childTail;
+                }
+            } else{
+                cur = cur.next;
+            }
+        }
+        return head;
+    }
+
+    public Node flattenRecursive(Node head) {
+
+        if(head == null) return head;
+
+        if(head.child!=null){
+            Node temp = head.next;
+            Node childHead = flattenRecursive(head.child);
+            head.next = childHead;
+            head.child = null;
+            childHead.prev = head;
+
+            Node cur = childHead;
+            while(cur.next!=null){
+                cur = cur.next;
+            }
+
+            if(temp!=null){
+                cur.next = temp;
+                temp.prev = cur;
+            }
+
+        } else{
+            head.next = flattenRecursive(head.next);
+        }
+
+        return head;
+    }
+}
