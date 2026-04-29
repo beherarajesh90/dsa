b581fab1f1f467575efc0fadf1ae1bd186ac1d8d
Rajesh <beherarajesh90@gmail.com>
2026-04-29 12:56:06 +0530

    linked list: stacks and queues -> ImplementStackUsingQueues

---
+++ src/main/java/com/interviewprep/dsa/stacksAndQueues/designProblems/ImplementStackUsingQueues.java
@@ -0,0 +1,34 @@
+package com.interviewprep.dsa.stacksAndQueues.designProblems;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ImplementStackUsingQueues {
+
+}
+
+class MyStack {
+    Queue<Integer> q;
+    public MyStack() {
+        q=new LinkedList<>();
+    }
+
+    public void push(int x) {
+        q.offer(x);
+        for(int i=0;i<q.size()-1;i++){
+            q.offer(q.poll());
+        }
+    }
+
+    public int pop() {
+        return q.poll();
+    }
+
+    public int top() {
+        return q.peek();
+    }
+
+    public boolean empty() {
+        return q.isEmpty();
+    }
+}
