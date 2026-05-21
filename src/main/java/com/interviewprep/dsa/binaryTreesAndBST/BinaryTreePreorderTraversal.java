415260cd29b11b05652c9d4dfcc53b771d7e0a0e
Rajesh <beherarajesh90@gmail.com>
2026-05-21 16:46:38 +0530

    binary trees and BST: traversal -> BinaryTreePreorderTraversal

--- src/main/java/com/interviewprep/dsa/binaryTreesAndBST/BinaryTreePreorderTraversal.java
+++ src/main/java/com/interviewprep/dsa/binaryTreesAndBST/BinaryTreePreorderTraversal.java
@@ -2,9 +2,10 @@ package com.interviewprep.dsa.binaryTreesAndBST;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class BinaryTreePreorderTraversal {
-    public List<Integer> preorderTraversal(TreeNode root) {
+    public List<Integer> preorderTraversalRecursive(TreeNode root) {
         List<Integer> result = new ArrayList<>();
         preorder(root, result);
         return result;
@@ -15,5 +16,22 @@ public class BinaryTreePreorderTraversal {
         result.add(root.val);
         preorder(root.left, result);
         preorder(root.right, result);
+    }
+
+    //iterative
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode current = root;
+        while (current!=null || !stack.isEmpty()){
+            while (current!=null){
+                result.add(current.val);
+                stack.push(current);
+                current = current.left;
+            }
+            current = stack.pop();
+            current = current.right;
+        }
+        return result;
     }
 }
