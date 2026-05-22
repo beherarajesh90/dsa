9a7ec9f62575ef9555ee17c7611c41d9cb8b6121
Rajesh <beherarajesh90@gmail.com>
2026-05-22 12:55:30 +0530

    binary trees and BST: mirror and symmetry -> LowestCommonAncestorOfABinarySearchTree

---
+++ src/main/java/com/interviewprep/dsa/binaryTreesAndBST/traversalAndSearch/LowestCommonAncestorOfABinarySearchTree.java
@@ -0,0 +1,34 @@
+package com.interviewprep.dsa.binaryTreesAndBST.traversalAndSearch;
+
+import com.interviewprep.dsa.binaryTreesAndBST.traversal.TreeNode;
+
+public class LowestCommonAncestorOfABinarySearchTree {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        // Case 1: both nodes are smaller
+        if (p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }
+
+        // Case 2: both nodes are greater
+        if (p.val > root.val && q.val > root.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }
+
+        // Split point => current node is LCA
+        return root;
+    }
+
+    public TreeNode lowestCommonAncestorIterative(TreeNode root, TreeNode p, TreeNode q) {
+        TreeNode cur = root;
+        while(cur!=null){
+            if(p.val < cur.val && q.val < cur.val){
+                cur = cur.left;
+            } else if(p.val > cur.val && q.val > cur.val){
+                cur = cur.right;
+            } else{
+                return cur;
+            }
+        }
+        return null;
+    }
+}
