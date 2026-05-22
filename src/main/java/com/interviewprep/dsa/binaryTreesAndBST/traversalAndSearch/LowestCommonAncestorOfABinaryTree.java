02d33761da9b8a6594c80c74a739ca786c7eeaf0
Rajesh <beherarajesh90@gmail.com>
2026-05-22 12:14:32 +0530

    binary trees and BST: mirror and symmetry -> LowestCommonAncestorOfABinaryTree

--- src/main/java/com/interviewprep/dsa/binaryTreesAndBST/traversalAndSearch/LowestCommonAncestorOfABinaryTree.java
+++ src/main/java/com/interviewprep/dsa/binaryTreesAndBST/traversalAndSearch/LowestCommonAncestorOfABinaryTree.java
@@ -5,7 +5,7 @@ import com.interviewprep.dsa.binaryTreesAndBST.traversal.TreeNode;
 //https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-tree/description/
 public class LowestCommonAncestorOfABinaryTree {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == null || root.val == p.val || root.val == q.val) return root;
+        if(root == null || root == p || root == q) return root;
 
         //find the nodes to left and right subtrees
         TreeNode leftNode = lowestCommonAncestor(root.left, p, q);
