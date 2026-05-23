90161fc9df5f950ccf24034eb50e4555dac3964b
Rajesh <beherarajesh90@gmail.com>
2026-05-23 11:56:04 +0530

    binary trees and BST: validation and properties -> BalancedBinaryTree

---
+++ src/main/java/com/interviewprep/dsa/binaryTreesAndBST/validationAndProperties/BalancedBinaryTree.java
@@ -0,0 +1,24 @@
+package com.interviewprep.dsa.binaryTreesAndBST.validationAndProperties;
+
+import com.interviewprep.dsa.binaryTreesAndBST.traversal.TreeNode;
+
+//https://leetcode.com/problems/balanced-binary-tree/description/
+public class BalancedBinaryTree {
+    public boolean isBalanced(TreeNode root) {
+        return getHeight(root) != -1;
+    }
+
+    private int getHeight(TreeNode root){
+        if(root == null) return 0;
+
+        int left = getHeight(root.left);
+        if(left == -1) return -1;
+
+        int right = getHeight(root.right);
+        if(right == -1) return -1;
+
+        if(Math.abs(left - right) > 1) return -1;
+
+        return 1 + Math.max(left, right);
+    }
+}
