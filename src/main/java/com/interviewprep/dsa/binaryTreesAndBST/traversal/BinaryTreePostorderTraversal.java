d57de4e75ddbe416c971b75d2f671152f372c366
Rajesh <beherarajesh90@gmail.com>
2026-05-25 17:20:57 +0530

    binary trees and BST: traversal -> BinaryTreePostorderTraversal

--- src/main/java/com/interviewprep/dsa/binaryTreesAndBST/traversal/BinaryTreePostorderTraversal.java
+++ src/main/java/com/interviewprep/dsa/binaryTreesAndBST/traversal/BinaryTreePostorderTraversal.java
@@ -2,9 +2,10 @@ package com.interviewprep.dsa.binaryTreesAndBST.traversal;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class BinaryTreePostorderTraversal {
-    public List<Integer> postorderTraversal(TreeNode root) {
+    public List<Integer> postorderTraversalRecursive(TreeNode root) {
         List<Integer> result = new ArrayList<>();
         postorder(root, result);
         return result;
@@ -15,5 +16,31 @@ public class BinaryTreePostorderTraversal {
         postorder(root.left, result);
         postorder(root.right, result);
         result.add(root.val);
+    }
+
+    //iterative
+    public List<Integer> postOrderIterative(TreeNode root){
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode current = root, lastVisited = null;
+
+        while (!stack.isEmpty() || current!=null){
+            while (current!=null){
+                stack.push(current);
+                current = current.left;
+            }
+
+            TreeNode peekNode = stack.peek();
+
+            //if peek node has right and not processed
+            if(peekNode.right!=null && lastVisited != peekNode.right){
+                current = peekNode.right;
+            } else {
+                result.add(peekNode.val);
+                lastVisited = stack.pop();
+            }
+        }
+
+        return result;
     }
 }
