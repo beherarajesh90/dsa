04858b7d9aac2b2329ea428dac37e71c67300c37
Rajesh <beherarajesh90@gmail.com>
2026-05-27 21:03:36 +0530

    binary trees and BST: construction -> ConstructBinaryTreeFromPreorderAndPostorderTraversal

---
+++ src/main/java/com/interviewprep/dsa/binaryTreesAndBST/construction/ConstructBinaryTreeFromPreorderAndPostorderTraversal.java
@@ -0,0 +1,51 @@
+package com.interviewprep.dsa.binaryTreesAndBST.construction;
+
+import com.interviewprep.dsa.binaryTreesAndBST.traversal.TreeNode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//https://leetcode.com/problems/construct-binary-tree-from-preorder-and-postorder-traversal/
+public class ConstructBinaryTreeFromPreorderAndPostorderTraversal {
+    int preIndex = 0;
+    Map<Integer, Integer> map = new HashMap<>();
+
+    public TreeNode constructFromPrePost(int[] preorder, int[] postorder) {
+
+        for (int i = 0; i < postorder.length; i++) {
+            map.put(postorder[i], i);
+        }
+
+        return build(preorder, postorder, 0, postorder.length - 1);
+    }
+
+    private TreeNode build(int[] preorder, int[] postorder,
+                           int postStart, int postEnd) {
+
+        if (postStart > postEnd)
+            return null;
+
+        // current root
+        TreeNode root = new TreeNode(preorder[preIndex++]);
+
+        // leaf node
+        if (postStart == postEnd)
+            return root;
+
+        // next preorder element is left child
+        int leftRoot = preorder[preIndex];
+
+        // find left child in postorder
+        int index = map.get(leftRoot);
+
+        // build left subtree
+        root.left = build(preorder, postorder,
+                postStart, index);
+
+        // build right subtree
+        root.right = build(preorder, postorder,
+                index + 1, postEnd - 1);
+
+        return root;
+    }
+}
