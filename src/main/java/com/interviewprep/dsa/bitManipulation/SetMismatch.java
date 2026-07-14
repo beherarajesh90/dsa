68f5def57c47296a6e2d956e661ba68eacdc2241
Rajesh <beherarajesh90@gmail.com>
2026-07-14 16:32:50 +0530

    bit manipulation -> SetMismatch

--- src/main/java/com/interviewprep/dsa/bitManipulation/SetMismatch.java
+++ src/main/java/com/interviewprep/dsa/bitManipulation/SetMismatch.java
@@ -2,8 +2,14 @@ package com.interviewprep.dsa.bitManipulation;
 
 //https://leetcode.com/problems/set-mismatch/description/
 public class SetMismatch {
-    // negative marking - optimal
     public int[] findErrorNums(int[] nums) {
+        // return findErrorNumsBitManipulation(nums);
+
+        return findErrorNumsOptimal(nums);
+    }
+
+    // negative marking - optimal
+    private int[] findErrorNumsOptimal(int[] nums) {
         int n = nums.length;
         int duplicate = -1, missing = -1;
 
@@ -23,6 +29,50 @@ public class SetMismatch {
             if (nums[i] > 0) {
                 missing = i + 1;
             }
+        }
+
+        return new int[]{duplicate, missing};
+    }
+
+    private int[] findErrorNumsBitManipulation(int[] nums) {
+        int n = nums.length;
+        int xor = 0;
+        for(int i=0; i<n; i++){
+            xor = xor ^ (i+1) ^ nums[i];
+        }
+
+        // The rightmost set bit is used because it guarantees that the duplicate and missing numbers end up in different groups.
+        int lsb = xor & -xor;
+
+        int g1 = 0;
+        int g2 = 0;
+
+        for(int i=0; i<n; i++){
+            if((nums[i] & lsb) != 0){
+                g1 ^= nums[i];
+            } else{
+                g2 ^= nums[i];
+            }
+
+            if(((i+1) & lsb) != 0){
+                g1 ^= i+1;
+            } else{
+                g2 ^= i+1;
+            }
+        }
+
+        int duplicate = -1, missing = -1;
+        for(int num: nums){
+            if(num == g1){
+                duplicate = g1;
+            }
+        }
+
+        if(duplicate == -1){
+            missing = g1;
+            duplicate = g2;
+        } else{
+            missing = g2;
         }
 
         return new int[]{duplicate, missing};
